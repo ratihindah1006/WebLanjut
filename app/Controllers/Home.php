6d77263494978741b9d111ff551aa3659621b2fb
ratih <ratihindah1006@gmail.com>
2020-10-17 08:41:22 +0700

    second commit

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,15 @@ class Home extends BaseController
 	{
 		return view('welcome_message');
 	}
+	public function show()
+	{
+		$data['nama']='Ratih Indah Wardani';
+		$data['npm']='1817051006';
+		$data['jurusan']='Ilmu Komputer';
+		echo view('mahasiswa/header');
+		echo view('mahasiswa/index',$data);
+		echo view('mahasiswa/footer');
+	}
 
 	//--------------------------------------------------------------------
 
